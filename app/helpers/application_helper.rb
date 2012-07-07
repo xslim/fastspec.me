8f2244477803242e9a32d8f1f7cfc0ab1f222d35
Taras Kalapun <t.kalapun@gmail.com>
2012-07-08 01:50:07 +0200

    More nicer navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  def current_team
+    return nil if !current_user
+
+    team_id = session[:team_id]
+    #@teams = current_user.teams
+    
+    team = (team_id) ? current_user.teams.find(team_id) : current_user.teams.first
+    return (team) ? team : nil
+  end
+
 end
