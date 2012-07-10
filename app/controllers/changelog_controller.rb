f1d8eb60229d19178882e08119cd5c6467524d4b
Taras Kalapun <t.kalapun@gmail.com>
2012-07-10 13:48:34 +0200

    More nicer Changelog

--- app/controllers/changelog_controller.rb
+++ app/controllers/changelog_controller.rb
@@ -1,10 +1,18 @@
 class ChangelogController < ApplicationController
 
   def index
-    @log = `git log --pretty=format:"%h|%an|%ar|%s" -n 20`
-    @log = @log.split("\n")
-    @log = @log.select { |e| e !~ /Merge|github/ }
-
+    @log = `git log --pretty=format:"%ae|%an|%h|%ar|%s" -n 20`
+    @log = @log.scan(/(.+)\|(.+)\|(.+)\|(.+)\|(.+)/).map do |(e,n,h,t,m)| 
+      next if m =~ /Merge|github/
+      {
+        email: e,
+        name: n,
+        hash: h,
+        time: t,
+        message: m
+      }
+    end
+    @log.compact!
 
   end
 
