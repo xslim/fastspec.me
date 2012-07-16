fb39b6aecd83bbd1864249cd6c80533829d2a2c4
Taras Kalapun <t.kalapun@gmail.com>
2012-07-16 21:56:45 +0200

    Symlinking uploads while deploying

--- config/deploy.rb
+++ config/deploy.rb
@@ -142,3 +142,44 @@ namespace :rvm do
   end
 end
 
+# ==============================
+# Uploads
+# ==============================
+
+namespace :uploads do
+
+  desc <<-EOD
+    Creates the upload folders unless they exist
+    and sets the proper upload permissions.
+  EOD
+  task :setup, :except => { :no_release => true } do
+    dirs = uploads_dirs.map { |d| File.join(shared_path, d) }
+    run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
+  end
+
+  desc <<-EOD
+    [internal] Creates the symlink to uploads shared folder
+    for the most recently deployed version.
+  EOD
+  task :symlink, :except => { :no_release => true } do
+    run "rm -rf #{release_path}/public/uploads"
+    run "ln -nfs #{shared_path}/#{stage}/uploads #{release_path}/public/uploads"
+  end
+
+  desc <<-EOD
+    [internal] Computes uploads directory paths
+    and registers them in Capistrano environment.
+
+    Note. I can't set value for directories directly in the code because
+    I don't know in advance selected stage.
+  EOD
+  task :register_dirs do
+    set :uploads_dirs,    %w(uploads uploads/partners).map { |d| "#{stage}/#{d}" }
+    set :shared_children, fetch(:shared_children) + fetch(:uploads_dirs)
+  end
+
+  after       "deploy:finalize_update", "uploads:symlink"
+  on :start,  "uploads:register_dirs",  :except => stages + ['multistage:prepare']
+
+end
+
